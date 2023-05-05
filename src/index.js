67c4308d615ea7e27a11a0352f6137c61bd44e8e
YuChih.Hu <hijac.hu@gmail.com>
2023-05-05 09:52:34 +0800

    3. Webpack Dev Server & Hot Module Reloading (HMR)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('hello from webpack');
+console.log('hello from webpack dev server');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        })
+    ]
 }
